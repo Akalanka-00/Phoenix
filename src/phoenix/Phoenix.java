a97c8fbe2dbce848f10667974c4161f0b172215e
Shenal Akalanka <shenalakalanka513@gmail.com>
2022-05-13 11:45:31 +0530

    Initial commit

---
+++ src/phoenix/Phoenix.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package phoenix;
+
+/**
+ *
+ * @author Lucifer
+ */
+public class Phoenix {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
+/**
+ *  try {
+            splashScreen sp = new splashScreen();
+            sp.setVisible(true);
+            for (int i = 0; i <= 100; i++) {
+
+                Thread.sleep(70);
+                sp.progressBar.setValue(i);
+
+            }
+        } catch (InterruptedException ex) {
+            Logger.getLogger(splashScreen.class.getName()).log(Level.SEVERE, null, ex);
+        }
+ */
