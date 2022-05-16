b7e00adc6dcd47bbd200cb78b1e5fa1ae47b8c06
Shenal Akalanka <shenalakalanka513@gmail.com>
2022-05-17 01:29:24 +0530

    sql
    Co-Authored-By: Chanaka-7518 <105494295+Chanaka-7518@users.noreply.github.com>
    Co-Authored-By: Hasitha Saubhagya Dassanayaka <79803555+HasithaSawbhagy@users.noreply.github.com>
    Co-Authored-By: Monali Thennakoon <105496631+Monali-MST@users.noreply.github.com>
    Co-Authored-By: Nethmi-24 <105492800+Nethmi-24@users.noreply.github.com>
    Co-Authored-By: Kosala Gayani <105628874+kosala509@users.noreply.github.com>
    Co-Authored-By: mahanama77 <105579102+mahanama77@users.noreply.github.com>
    Co-Authored-By: sandu345 <105628881+sandu345@users.noreply.github.com>
    Co-Authored-By: Thushan D. Fernando <thushandfdo@gmail.com>

---
+++ src/com/phoenix/classes/conSQL.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.phoenix.classes;
+
+/**
+ *
+ * @author Lucifer
+ */
+public class conSQL {
+    
+    String user = "root";
+    String password = "";
+    String url = "jdbc:mysql://localhost:3306/mysql?zeroDateTimeBehavior=CONVERT_TO_NULL";
+   // Class.forName("com.mysql.cj.jdbc.Driver");
+    
+}
