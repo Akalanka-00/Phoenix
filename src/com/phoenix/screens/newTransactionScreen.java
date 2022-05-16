88bbe470ef608c577fe64e9349bcc445ace9e9fa
Shenal Akalanka <shenalakalanka513@gmail.com>
2022-05-16 17:02:07 +0530

    add
    Co-Authored-By: Chanaka-7518 <105494295+Chanaka-7518@users.noreply.github.com>
    Co-Authored-By: Hasitha Saubhagya Dassanayaka <79803555+HasithaSawbhagy@users.noreply.github.com>
    Co-Authored-By: Monali Thennakoon <105496631+Monali-MST@users.noreply.github.com>
    Co-Authored-By: Nethmi-24 <105492800+Nethmi-24@users.noreply.github.com>
    Co-Authored-By: Kosala Gayani <105628874+kosala509@users.noreply.github.com>
    Co-Authored-By: mahanama77 <105579102+mahanama77@users.noreply.github.com>
    Co-Authored-By: sandu345 <105628881+sandu345@users.noreply.github.com>
    Co-Authored-By: Thushan D. Fernando <thushandfdo@gmail.com>

--- src/com/phoenix/screens/newTransactionScreen.java
+++ src/com/phoenix/screens/newTransactionScreen.java
@@ -183,6 +183,11 @@ public class newTransactionScreen extends javax.swing.JFrame {
         });
 
         jButton3.setText("Cancel");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -253,6 +258,11 @@ public class newTransactionScreen extends javax.swing.JFrame {
         newAccScreen accScreen = new newAccScreen();
         accScreen.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        this.setVisible(false);
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
